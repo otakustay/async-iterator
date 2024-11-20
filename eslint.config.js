107d89bbc0b2c8cf76dd5b634fa2231fd3412d04
Gray Zhang <otakustay@gmail.com>
2024-11-20 14:56:33 +0800

    build: initialize repository

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+    js.configs.recommended,
+    ...ts.configs.strictTypeChecked,
+    ...ts.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+        },
+    }
+);
