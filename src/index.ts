7019b050f621bd0a9e72dab6b09757e218d2c17e
otakustay <otakustay@gmail.com>
2024-11-29 16:28:34 +0800

    feat: export all factory functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export {AsyncIteratorController} from './controller.js';
+export {fromIntervalEach} from './factory/array.js';
+export {fromEvent} from './factory/event.js';
+export {fromStreamReader} from './factory/reader.js';
+export {fromStream} from './factory/stream.js';
+
+export type {EventDescription, EventEmitter} from './factory/event.js';
